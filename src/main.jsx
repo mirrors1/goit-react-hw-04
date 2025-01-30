82ac97db028e7420778c6dbe1291d8b1a99eef12
Oleksandr Prokopchuk <sasha195@i.ua>
2025-01-30 10:09:04 +0200

    Add modern-normalize

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+// Імпорт стилів нормалізації
+import 'modern-normalize';
 import './index.css';
 import App from './components/App';
 import { Toaster } from 'react-hot-toast';
