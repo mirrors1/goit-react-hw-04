197013c861597a3dc1bb1ce11d5942e3c68216a1
Oleksandr Prokopchuk <sasha195@i.ua>
2025-01-30 10:04:04 +0200

    initial-commit

---
+++ src/components/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react';
+import toast from 'react-hot-toast';
+
+function App() {
+  toast.success(`ALl filters and pages drop to init values`);
+  return (
+    <>
+      <h1>goit-react-hw-04</h1>
+    </>
+  );
+}
+
+export default App;
