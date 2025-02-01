c98cada7ad23f7697dd9c8e0914ca3021a5b04c6
Oleksandr Prokopchuk <sasha195@i.ua>
2025-02-02 01:32:38 +0200

    Add components

--- src/main.jsx
+++ src/main.jsx
@@ -9,6 +9,6 @@ import { Toaster } from 'react-hot-toast';
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
-    <Toaster position="top-right" toastOptions={{ duration: 1000 }} />
+    <Toaster position="top-right" toastOptions={{ duration: 2000 }} />
   </StrictMode>
 );
