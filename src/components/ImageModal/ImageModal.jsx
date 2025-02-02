2e3bffa17df56ce244a7eef57c1baa8ce74bd123
Oleksandr Prokopchuk <sasha195@i.ua>
2025-02-02 08:48:57 +0200

    correct modal style from mobile

--- src/components/ImageModal/ImageModal.jsx
+++ src/components/ImageModal/ImageModal.jsx
@@ -10,14 +10,16 @@ const customStyles = {
     marginRight: '-50%',
     transform: 'translate(-50%, -50%)',
     backgroundColor: `VAR(--color-green-300)`,
+    padding: 0,
+    border: 'none',
   },
   overlay: {
     backgroundColor: 'rgba(0 , 0 , 0 , 0.6)',
-    position: `fixed`,
-    top: 0,
-    left: 0,
-    right: 0,
-    bottom: 0,
+    // position: `fixed`,
+    // top: 0,
+    // left: 0,
+    // right: 0,
+    // bottom: 0,
   },
 };
 Modal.setAppElement('#root');
